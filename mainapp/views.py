3b77f55fc357e1aa51d4dfe208b20740f78da4fd
MishchenkoDmitry <yasher2000@mail.ru>
2019-04-07 22:58:16 +0300

    3 урок дополнение

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, ProductCategory
 
 def main(request):
+    category_list = ProductCategory.objects.all()
     product_list = Product.objects.all()
-    return render(request, 'mainapp/index.html',context={'products': product_list})
+    return render(request, 'mainapp/index.html',context={'products': product_list, 'categorys': category_list})
 
 def products(request, pk=None):
     return render(request, 'mainapp/products.html')
